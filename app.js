37c9c986ff8e803eeb100989978300909517a514
lermintech-cloud <lermintech@gmail.com>
2026-07-20 13:47:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const API =
-"https://script.google.com/macros/s/AKfycbx8PJDCDw7qSegH9chbQkjZSxVsx1-GtZZvqnqMQcX1aDZx2u13ECFIlL6OVEytqdfdEg/exec?action=dashboard_v2";
+const API="https://script.google.com/macros/s/AKfycbx8PJDCDw7qSegH9chbQkjZSxVsx1-GtZZvqnqMQcX1aDZx2u13ECFIlL6OVEytqdfdEg/exec?action=dashboard_v2";
+
+const tg=window.Telegram.WebApp;
+
+tg.expand();
 
 fetch(API)
+
 .then(r=>r.json())
+
 .then(data=>{
 
-    document.getElementById("weather").innerHTML=data.weather;
+document.getElementById("datetime").innerHTML=data.datetime;
 
-    document.getElementById("pm25").innerHTML=data.pm25;
+document.getElementById("weather").innerHTML=data.weather;
+
+document.getElementById("pm25").innerHTML=data.pm25;
 
 });
